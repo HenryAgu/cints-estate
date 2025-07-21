1b28629214743f711b41ddc105e22ed4b75d4180
Henry Agu <aguhenrychuks@gmail.com>
2025-07-21 21:56:01 +0100

    feat: key service component for mobile

--- src/components/shared/keyService.tsx
+++ src/components/shared/keyService.tsx
@@ -8,19 +8,41 @@ const HelveticaNeue = localFont({
 
 const KeyService = () => {
   return (
-    <section className="relative bg-[url(/images/key-service.png)] bg-no-repeat bg-cover max-h-[597px] container w-full mx-auto">
+    <section className="relative bg-[url(/images/key-service.png)] bg-no-repeat bg-cover max-h-full lg:max-h-[597px] container w-full mx-auto">
       <div className="absolute inset-0 bg-black/75"></div>
       <div className="relative z-10 p-20 flex flex-col items-center">
         <div className="text-white text-center flex flex-col gap-y-4">
-          <p className={`text-xl ${HelveticaNeue.className} font-normal`}>
+          <p className={`text-lg lg:text-xl ${HelveticaNeue.className} font-normal`}>
             Key Services
           </p>
-          <h3 className="font-semibold text-[40px] leading-[130%]">What We Get to Offer <br /> you at Cints Estate</h3>
+          <h3 className="font-semibold text-4xl lg:text-[40px] lg:leading-[130%]">
+            What We Get to Offer <br /> you at Cints Estate
+          </h3>
         </div>
-        <div className="mt-20 flex items-center gap-x-24.5">
-          <Image src="/icons/shortlet.svg" alt="icon" width={131} height={115} className="aspect-[131/115]"/>
-          <Image src="/icons/buy.svg" alt="icon" width={179} height={115} className="aspect-[179/115]"/>
-          <Image src="/icons/sell.svg" alt="icon" width={147} height={115} className="aspect-[147/115]"/>
+        <div className="mt-10 lg:mt-20 flex lg:flex-row flex-col items-center gap-y-10 gap-x-24.5">
+          <Image
+            src="/icons/shortlet.svg"
+            alt="icon"
+            width={131}
+            height={115}
+            className="aspect-[65/57] lg:aspect-[131/115]"
+          />
+
+          <Image
+            src="/icons/buy.svg"
+            alt="icon"
+            width={179}
+            height={115}
+            className="aspect-[65/57] lg:aspect-[179/115]"
+          />
+
+          <Image
+            src="/icons/sell.svg"
+            alt="icon"
+            width={147}
+            height={115}
+            className="aspect-[65/57] lg:aspect-[147/115]"
+          />
         </div>
       </div>
     </section>
