1a7c06a21b784d5549e3bc5437316b4c2b7f46a6
Henry Agu <aguhenrychuks@gmail.com>
2025-07-22 01:12:46 +0100

    style: key services upgrade

--- src/components/home/propertyCard.tsx
+++ src/components/home/propertyCard.tsx
@@ -48,7 +48,7 @@ const properties: Property[] = [
 
 const PropertyCard = () => {
   return (
-    <div className="grid grid-col-1 lg:grid-cols-3 gap-x-5 gap-y-10 mt-14 px-5">
+    <div className="grid grid-col-1 lg:grid-cols-3 gap-x-5 gap-y-10 mt-14">
       {properties.map((item, index) => (
         <div
           className="border border-[#E4DCD1] rounded-[20px] p-5 flex flex-col gap-y-5"
--- src/components/shared/keyService.tsx
+++ src/components/shared/keyService.tsx
@@ -19,13 +19,13 @@ const KeyService = () => {
             What We Get to Offer you at Cints Estate
           </h3>
         </div>
-        <div className="mt-10 lg:mt-20 grid grid-cols-2 lg:grid-cols-3 gap-y-10 gap-x-24.5">
+        <div className="mt-10 lg:mt-20 grid grid-cols-3 gap-x-10 lg:gap-x-24.5">
           <Image
             src="/icons/shortlet.svg"
             alt="icon"
             width={131}
             height={115}
-            className="aspect-[131/115]"
+            className="aspect-[131/115] lg:w-[131px] lg:h-[115px]"
           />
 
           <Image
@@ -33,7 +33,7 @@ const KeyService = () => {
             alt="icon"
             width={179}
             height={115}
-            className="aspect-[179/115]"
+            className="aspect-[179/115] lg:w-[179px] lg:h-[115px]"
           />
 
           <Image
@@ -41,7 +41,7 @@ const KeyService = () => {
             alt="icon"
             width={147}
             height={115}
-            className="aspect-[147/115]"
+            className="aspect-[147/115] lg:w-[147px] lg:h-[115px]"
           />
         </div>
       </div>
