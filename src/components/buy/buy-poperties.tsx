2f860fb730a6c96c0d4339209eed5c6b87411a79
Henry Agu <aguhenrychuks@gmail.com>
2025-07-23 09:19:49 +0100

    fix: mobile screens

--- src/components/buy/buy-poperties.tsx
+++ src/components/buy/buy-poperties.tsx
@@ -116,10 +116,10 @@ const properties: Property[] = [
 const BuyProperties = () => {
   return (
     <section className="mx-4 lg:mx-20">
-      <h1 className="text-brand-neutral-500 font-bold text-[32px] leading-[130%]">
+      <h1 className="text-brand-neutral-500 font-bold text-[28px] lg:text-[32px] lg:leading-[130%]">
         Buy Properties
       </h1>
-      <div className="grid grid-col-1 lg:grid-cols-3 gap-5 mt-10 mb-20">
+      <div className="grid grid-col-1 lg:grid-cols-3 gap-5 mt-10 mb-10 lg:mb-20">
         {properties.map((item, index) => (
           <PropertyCard item={item} key={index} />
         ))}
