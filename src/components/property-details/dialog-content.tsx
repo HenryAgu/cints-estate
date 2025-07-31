cf0c4ec50cbb703a6e8a607c3187407104e52a6d
Henry Agu <aguhenrychuks@gmail.com>
2025-07-31 15:46:07 +0100

    feat: dialog added

---
+++ src/components/property-details/dialog-content.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DialogContent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DialogContent
--- src/components/property-details/property-image-grid.tsx
+++ src/components/property-details/property-image-grid.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
 
 const gridImages: { src: string; alt: string }[] = [
   { src: "/images/grid2.png", alt: "grid2" },
@@ -12,39 +18,46 @@ const gridImages: { src: string; alt: string }[] = [
 const PropertyImageGrid = () => {
   return (
     <section className="relative lg:mb-10 my-5 md:mt-5 mx-4 lg:mx-20 flex md:flex-row flex-col gap-4 lg:gap-5 h-full">
-      <div className="w-full basis-[50%]">
-        <Image
-          src="/images/grid1.png"
-          alt="grid-image"
-          width={630}
-          height={592}
-          className="aspect-[630/592] w-full h-full"
-        />
-      </div>
-      <div className="w-full basis-[50%] grid grid-cols-2 gap-4 lg:gap-5">
-        {gridImages.map((image, index) => (
+      <Dialog>
+        <div className="w-full basis-[50%]">
           <Image
-            key={index}
-            src={image.src}
-            alt={image.alt}
-            width={309}
-            height={288}
-            className="aspect-square w-full h-full"
+            src="/images/grid1.png"
+            alt="grid-image"
+            width={630}
+            height={592}
+            className="aspect-[630/592] w-full h-full"
           />
-        ))}
-      </div>
-      <div className="absolute bottom-4 right-4">
-        <Button type="button" variant="ghost" className="cursor-pointer p-2.5 bg-white rounded-[10px]">
-          <Image
-            src="/icons/camera.svg"
-            width={24}
-            height={24}
-            alt="camera_icon"
-            className="w-6 h-6 aspect-square"
-          />
-          <span>2/7</span>
-        </Button>
-      </div>
+        </div>
+        <div className="w-full basis-[50%] grid grid-cols-2 gap-4 lg:gap-5">
+          {gridImages.map((image, index) => (
+            <Image
+              key={index}
+              src={image.src}
+              alt={image.alt}
+              width={309}
+              height={288}
+              className="aspect-square w-full h-full"
+            />
+          ))}
+        </div>
+        <div className="absolute bottom-4 right-4">
+          <DialogTrigger>
+            <DialogContent>
+              <DialogTitle>1</DialogTitle>
+            </DialogContent>
+            <div className="cursor-pointer flex items-center gap-x-2.5 p-2.5 bg-white rounded-[10px]">
+              <Image
+                src="/icons/camera.svg"
+                width={24}
+                height={24}
+                alt="camera_icon"
+                className="w-6 h-6 aspect-square"
+              />
+              <span>2/7</span>
+            </div>
+          </DialogTrigger>
+        </div>
+      </Dialog>
     </section>
   );
 };
